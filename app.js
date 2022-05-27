3c200e98ee075387341d9d9cdeb820db5ba668e2
KiyomiDev <amal.97mo@gmail.com>
2022-05-27 14:16:59 +0200

    Show new text on page load

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-let randomParagraph = 'https://api.quotable.io/random?minLength=200';
+let randomParagraph = 'https://api.quotable.io/random?minLength=200',
+    pContainer = document.querySelector('.paragraph-container')
+
+showNewParagraph();
 
 function getRandomParagraph() {
    return fetch(randomParagraph)
    .then(response => response.json())
    .then(data => data.content);
+}
+
+async function showNewParagraph() {
+   pContainer.innerHTML = ''
+   let paragraph = await getRandomParagraph();
+   paragraph.split(' ').forEach(word => {
+      let wordContainer = document.createElement('div');
+
+      word.split('').forEach(char => {
+         let character = document.createElement('span');
+         character.innerText = char;
+         wordContainer.appendChild(character);
+      })
+      pContainer.appendChild(wordContainer);
+   })
 }
